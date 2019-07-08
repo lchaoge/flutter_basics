6e67d795187f7d743bfcd64f357b6c21bdb3919b
liuyong <liuyong@wumart.com>
2019-07-08 13:43:22 +0800

    tab and navigation_bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_basics/model/post.dart';
+import 'package:flutter_basics/demo/listview_demo.dart';
+import 'package:flutter_basics/demo/bottom_navigation_bar_demo.dart';
 
 void main() => runApp(App());
 
@@ -7,56 +8,130 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(primarySwatch: Colors.yellow), home: Home());
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            primarySwatch: Colors.yellow,
+            highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
+            splashColor: Colors.white70),
+        home: Home());
   }
 }
 
 class Home extends StatelessWidget {
-  Widget _listItemBuilder(BuildContext context, int index) {
-    return Container(
-      color: Colors.white,
-      margin: EdgeInsets.all(8.0),
-      child: Column(
-        children: <Widget>[
-          Image.network(posts[index].imageUrl),
-          SizedBox(
-            height: 16.0,
-          ),
-          Text(posts[index].title, style: Theme.of(context).textTheme.title),
-          Text(posts[index].author, style: Theme.of(context).textTheme.subhead),
-          SizedBox(
-            height: 16.0,
-          ),
-        ],
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.grey[100],
-        appBar: AppBar(
-          title: Text('chaoge'),
-          elevation: 0.0,
-        ),
-        body: ListView.builder(
-          itemCount: posts.length,
-          itemBuilder: _listItemBuilder,
-        ));
-  }
-}
-
-class Hello extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'hello',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-            fontSize: 40.0, fontWeight: FontWeight.bold, color: Colors.yellow),
-      ),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+          backgroundColor: Colors.grey[100],
+          appBar: AppBar(
+            // leading: IconButton(
+            //     icon: Icon(Icons.menu),
+            //     tooltip: 'navigration',
+            //     onPressed: () => debugPrint('navigration button is pressed.')),
+            title: Text('chaoge'),
+            actions: <Widget>[
+              IconButton(
+                  icon: Icon(Icons.search),
+                  tooltip: 'search',
+                  onPressed: () => debugPrint('search button is pressed.')),
+            ],
+            elevation: 0.0,
+            bottom: TabBar(
+              unselectedLabelColor: Colors.black38,
+              indicatorColor: Colors.black54,
+              indicatorSize: TabBarIndicatorSize.label,
+              indicatorWeight: 1.0,
+              tabs: <Widget>[
+                Tab(
+                  icon: Icon(Icons.local_florist),
+                ),
+                Tab(
+                  icon: Icon(Icons.change_history),
+                ),
+                Tab(
+                  icon: Icon(Icons.directions_bike),
+                ),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              ListViewDemo(),
+              Icon(
+                Icons.change_history,
+                size: 128.0,
+                color: Colors.black12,
+              ),
+              Icon(
+                Icons.directions_bike,
+                size: 128.0,
+                color: Colors.black12,
+              ),
+            ],
+          ),
+          drawer: Drawer(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                UserAccountsDrawerHeader(
+                  accountName: Text('chaoge',
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                  accountEmail: Text('13260094140@163.com'),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        'http://i0.hdslb.com/bfs/face/452cef1ba6dd594d4c11ac19a633e747bed93acd.jpg@52w_52h.webp'),
+                  ),
+                  decoration: BoxDecoration(
+                      color: Colors.yellow[400],
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              'http://ww1.sinaimg.cn/large/663d3650gy1fplwwcynw2j20p00b4js9.jpg'),
+                          fit: BoxFit.cover,
+                          colorFilter: ColorFilter.mode(
+                              Colors.yellow[400].withOpacity(0.6),
+                              BlendMode.hardLight))),
+                ),
+                ListTile(
+                  title: Text(
+                    'Messages',
+                    textAlign: TextAlign.right,
+                  ),
+                  trailing: Icon(
+                    Icons.message,
+                    color: Colors.black12,
+                    size: 22.0,
+                  ),
+                  onTap: () => Navigator.pop(context),
+                ),
+                ListTile(
+                  title: Text(
+                    'Favorite',
+                    textAlign: TextAlign.right,
+                  ),
+                  trailing: Icon(
+                    Icons.favorite,
+                    color: Colors.black12,
+                    size: 22.0,
+                  ),
+                  onTap: () => Navigator.pop(context),
+                ),
+                ListTile(
+                  title: Text(
+                    'Settings',
+                    textAlign: TextAlign.right,
+                  ),
+                  trailing: Icon(
+                    Icons.settings,
+                    color: Colors.black12,
+                    size: 22.0,
+                  ),
+                  onTap: () => Navigator.pop(context),
+                ),
+              ],
+            ),
+          ),
+          bottomNavigationBar: BottomNavigationBarDemo()),
     );
   }
 }
