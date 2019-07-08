4dcaaacd330ee190c6c66e65ba7b92e5e07ebe0d
liuyong <liuyong@wumart.com>
2019-07-08 12:52:36 +0800

    listview

---
+++ lib/model/post.dart
@@ -0,0 +1,29 @@
+class Post{
+  final String title;
+  final String author;
+  final String imageUrl;
+
+  const Post({
+    this.title,
+    this.author,
+    this.imageUrl,
+  });
+}
+
+final List<Post> posts = [
+  Post(
+    title: 'chaoge',
+    author: 'chaoge',
+    imageUrl: 'http://ww1.sinaimg.cn/large/663d3650gy1fplwu9ze86j20m80b40t2.jpg'
+  ),
+  Post(
+    title: 'chaoge2',
+    author: 'chaoge2',
+    imageUrl: 'http://ww1.sinaimg.cn/large/663d3650gy1fplwvqwuoaj20xc0p0t9s.jpg'
+  ),
+  Post(
+    title: 'chaoge3',
+    author: 'chaoge3',
+    imageUrl: 'http://ww1.sinaimg.cn/large/663d3650gy1fplwwcynw2j20p00b4js9.jpg'
+  )
+];
